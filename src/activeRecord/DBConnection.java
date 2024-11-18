8a21374a94f14b0af9bd20d706cb660027d3d22a
Yeahboys04 <ryankorban2004@gmail.com>
2024-11-18 11:25:07 +0100

    Ajout du singleton

--- src/activeRecord/DBConnection.java
+++ src/activeRecord/DBConnection.java
@@ -1,13 +1,26 @@
 package activeRecord;
 
-public class DBConnection {
-    private DBConnection dbConnection;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
-    private DBConnection(DBConnection dbConnection){
-        this.dbConnection =  dbConnection;
+
+public class DBConnection {
+    private static DBConnection instance;
+
+    private Connection connection;
+    private String connectionProps;
+
+
+    private DBConnection() throws SQLException {
+        this.connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/activeRecord","korban2u", "Vachier2!");
     }
 
-    public DBConnection DBConnection(){
-
+    public static synchronized DBConnection getInstance() throws SQLException {
+        if(instance == null) instance = new DBConnection();
+        return instance;
+    }
+    public Connection getConnection(){
+        return connection;
     }
 }
