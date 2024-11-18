b81d081430152839bd805b4b5bd62f71b11d2add
LukaSalvo <luka.salvo23@gmail.com>
2024-11-18 11:48:22 +0100

    Modification Personne.java

--- src/activeRecord/Personne.java
+++ src/activeRecord/Personne.java
@@ -51,7 +51,7 @@ public class Personne {
 
     public static Personne findById(int id) throws SQLException {
         Connection dbconnection = DBConnection.getInstance().getConnection();
-        PreparedStatement stmt = dbconnection.createStatement();
+        Statement stmt = dbconnection.createStatement();
         String query = "SELECT * FROM Personne WHERE id =?";
 
 
