1653abe9ceff9610961f46b2b54567aa10b0b445
Yeahboys04 <ryankorban2004@gmail.com>
2024-11-18 11:12:57 +0100

    Ajout single

---
+++ src/activeRecord/DBConnection.java
@@ -0,0 +1,13 @@
+package activeRecord;
+
+public class DBConnection {
+    private DBConnection dbConnection;
+
+    private DBConnection(DBConnection dbConnection){
+        this.dbConnection =  dbConnection;
+    }
+
+    public DBConnection DBConnection(){
+
+    }
+}
